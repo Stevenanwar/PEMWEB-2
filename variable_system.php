a05c4c45517b7f9f658ecd6abc177165cb69b53e
Stevenanwar <fathananwar2115@gmail.com>
2025-02-20 12:19:32 +0700

    Praktikum 01

---
+++ variable_system.php
@@ -0,0 +1,4 @@
+<?php
+    echo 'dokumen root : ' . $_SERVER["DOCUMENT_ROOT"];
+    ECHO '<br/>Nama File ' .$_SERVER["PHP_SELF"];
+?>
---
+++ variable_user.php
@@ -0,0 +1,10 @@
+<?php
+$nama = 'fathan anwar';
+$umur = 18;
+$berat = 56.1;
+echo 'nama ' . $nama;
+echo '<br/>umur ' . $umur . ' Tahun';
+echo '<br/>berat ' . $berat . ' Kg'; 
+
+echo "<br/>Hello Apa $nama apa kabar?"; 
+?>
