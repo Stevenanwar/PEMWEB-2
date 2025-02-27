dd4af1cad2d2b0abe2aa20ab6ab3f6dbdf750539
Stevenanwar <fathananwar2115@gmail.com>
2025-02-27 21:51:41 +0700

    tugas praktikum2

---
+++ praktikum-2a/data-form-register.php
@@ -0,0 +1,16 @@
+<?php
+$ar_prodi = [
+    'SI' => 'Sistem Informasi' ,
+    'TI' => 'Teknik Informasi',
+];
+$ar_skill =[
+    'HTML' => 10,
+    'CSS' => 10 ,
+    'JavaScript' => 10 ,
+    'RWD Boostrap' => 20,
+    'PHP' => 30,
+    'Python' => 30,
+    'Java' => 50,
+];
+$ar_domisili = ["Jakarta" , "Bekasi" , "Tanggerang"  , "Depok", "Lainnya"];
+?>
